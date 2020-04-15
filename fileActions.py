04d8d6f104597f6538074208a9c0f0e62944af95
tomasz.zawadzki.eka <241503@student.pwr.edu.pl>
2020-04-15 20:19:11 +0200

    tomko - obsługa wykresu fazy (chyba dobrze)

--- fileActions.py
+++ fileActions.py
@@ -12,11 +12,13 @@ def executeNumpyFourierTransform(filename):
     f = np.fft.fft2(img)
     fshift = np.fft.fftshift(f)
     magnitude_spectrum = 20 * np.log(np.abs(fshift))
-
-    plt.subplot(121), plt.imshow(img, cmap='gray')
+    phase_spectrum = np.angle(fshift);
+    plt.subplot(131), plt.imshow(img, cmap='gray')
     plt.title('Input Image'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122), plt.imshow(magnitude_spectrum, cmap='gray')
+    plt.subplot(132), plt.imshow(magnitude_spectrum, cmap='gray')
     plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+    plt.subplot(133), plt.imshow(phase_spectrum, cmap='gray')
+    plt.title('Phase'), plt.xticks([]), plt.yticks([])
     plt.show()
 
 
